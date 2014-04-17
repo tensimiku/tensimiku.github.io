2b774585065c23e905112f6175d9084aa1dc1b99
tensimiku <tensimiku@gmail.com>
2014-04-18 01:50:03 +0900

    well
    zz

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,13 @@
-console.log('This would be the main JS file.');
+$(window).load(function () {
+    var i = 0;
+    var images = ['images/bg0.jpg', 'images/bg.jpg'];
+    var image = $('#header_wrap');
+    setInterval(function () {
+        image.fadeOut(1000, function () {
+            image.css('background-image', 'url(' + images[i++] + ')');
+            image.fadeIn(1000);
+        });
+        if (i == images.length)
+            i = 0;
+    }, 14000);
+});
