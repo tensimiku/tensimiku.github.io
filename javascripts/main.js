c90726a35a1f282304055c0a7cd67d1e232b0d75
tensimiku <tensimiku@gmail.com>
2014-04-18 09:25:56 +0900

    done

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,13 +1,27 @@
 $(window).load(function () {
+    
     var i = 0;
     var bgs = ['images/bg0.jpg', 'images/bg.jpg'];
     var bg = $('#header_wrap');
-    setInterval(function () {
+    $("#header_wrap").click(function () {
         bg.fadeOut(393, function () {
+            if (i >= 2)
+            i = 0;
+            bg.css('background-image', 'url(' + bgs[i++] + ')');
+        });
+        bg.fadeIn(393);
+    }
+    );
+    /*
+    setInterval(function () {
+
+        bg.fadeOut(393, function () {
+            if (i >= bgs.length)
+            i = 0;
             bg.css('background-image', 'url(' + bgs[i++] + ')');
             bg.fadeIn(393);
         });
-        if (i == bgs.length)
-            i = 0;
+
     }, 13939);
+    */
 });
